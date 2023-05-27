e14c9d5b27f6edebad3b0f679363f8f842c742dd
ANDRES <andresb@example.com>
2023-05-27 10:56:16 -0500

    Colocando las variables en el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,13 @@
-
+import React, {useState, useEffect} from "react"
+import { db } from "../firebase"
+import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc } from "firebase/firestore"
 
 const Formulario = () => {
   return (
     <div>Formulario</div>
   )
 }
+
+
 
 export default Formulario
