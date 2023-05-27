5ff85b37fd4766efbd9316d5ae371b37f77fa1b5
ANDRES <andresb@example.com>
2023-05-27 11:35:36 -0500

    Colocación de los inputs y listado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -14,8 +14,22 @@ const Formulario = () => {
         <div className="row">
             <div className="col-8">
                 <h4 className="text-center">Listado</h4>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <span className="lead">text</span>
+                        <button className="btn btn-danger btn-sm float-end mx-2">Eliminar</button>
+                        <button className="btn btn-warning btn-sm float-end">Editar</button>
+                    </li>
+                </ul>
+            </div>
+            <div className="col-4">
+                <h4 className="text-center">Agregar Manillas</h4>
+                <form>
+                    <input type="text" className="form-control mb-2" placeholder="Ingrese cantidad"/>
+                </form>
             </div>
         </div>
+        
     </div>
   )
 }
