a71b26f0a5a144177442e9c573a29a57e56b8c0c
ANDRES <andresb@example.com>
2023-05-27 11:14:27 -0500

    Creacion diseño del crud y sus valores inciales

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,8 +3,15 @@ import { db } from "../firebase"
 import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc } from "firebase/firestore"
 
 const Formulario = () => {
-  return (
-    <div>Formulario</div>
+    const [cantidad,setCantidad] = useState("");
+    const [valueMaterial, setValueMaterial] = useState("");
+    const [valueDije, setValueDije] = useState("");
+    const [valueTipo, setValueTipo] = useState("");
+    return (
+    <div className="container mt-5">
+        <h1 className="text-center">CRUD DE MANILLAS</h1>
+
+    </div>
   )
 }
 
